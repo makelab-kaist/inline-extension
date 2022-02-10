e852a95a37bf5abc6c568f736a2cccdf755ca28a
Andrea <andrea@kaist.ac.kr>
2022-02-10 13:02:03 +0900

    Added functions

--- templates/dependencies/_functions.h
+++ templates/dependencies/_functions.h
@@ -1,6 +1,7 @@
 #ifndef __DASH_FUNCTIONS__H__
 #define __DASH_FUNCTIONS__H__
 
+// See reference:  https://github.com/arduino/ArduinoCore-avr/blob/master/cores/arduino/Arduino.h
 
 #define PAUSE 5
 #define _Serial_println _Serial_print
@@ -53,7 +54,6 @@ public:
     Serial.println("");
   }
 };
-
 
 void printValueFormatted(const SerialWrapper &value, PARAMS)
 {
@@ -121,7 +121,6 @@ void _analogWrite(uint8_t pin, int value, PARAMS)
   printValueFormatted(SerialWrapper(perc), id, line, index, items);
 }
 
-
 // Time
 
 unsigned long _millis(PARAMS)
@@ -145,6 +144,12 @@ unsigned long _pulseIn(uint8_t pin, uint8_t state, unsigned long timeout, PARAMS
   return m;
 }
 
+unsigned long _pulseIn(uint8_t pin, uint8_t state, PARAMS)
+{
+  unsigned long m = pulseIn(pin, state, 1000000UL);
+  printValueFormatted(SerialWrapper(m), id, line, index, items);
+  return m;
+}
 
 // Random
 
@@ -162,86 +167,99 @@ long _random(long minvalue, long maxvalue, PARAMS)
   return r;
 }
 
-long _map(long x, long in_min, long in_max, long out_min, long out_max, PARAMS) {
+long _map(long x, long in_min, long in_max, long out_min, long out_max, PARAMS)
+{
   long m = map(x, in_min, in_max, out_min, out_max);
   printValueFormatted(SerialWrapper(m), id, line, index, items);
   return m;
 }
 
-
 // Math
 
-long _min (long a, long b, PARAMS){
-  long r = min(a,b);
+long _min(long a, long b, PARAMS)
+{
+  long r = min(a, b);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
-double _min (double a, double b, PARAMS){
-  double r = min(a,b);
-  printValueFormatted(SerialWrapper(r), id, line, index, items);
-  return r;
-}
-
-long _max (long a, long b, PARAMS){
-  long r = max(a,b);
+double _min(double a, double b, PARAMS)
+{
+  double r = min(a, b);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-double _max (double a, double b, PARAMS){
-  double r = max(a,b);
+long _max(long a, long b, PARAMS)
+{
+  long r = max(a, b);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-long _abs (long a, PARAMS){
+double _max(double a, double b, PARAMS)
+{
+  double r = max(a, b);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
+
+long _abs(long a, PARAMS)
+{
   long r = abs(a);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
-double _abs (double a, PARAMS){
+double _abs(double a, PARAMS)
+{
   double r = abs(a);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-long _round (double a, PARAMS){
+long _round(double a, PARAMS)
+{
   long r = round(a);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
-  return r; 
+  return r;
 }
 
-double _radians (double deg, PARAMS){
+double _radians(double deg, PARAMS)
+{
   double r = radians(deg);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-double _degrees (double rad, PARAMS){
+double _degrees(double rad, PARAMS)
+{
   double r = degrees(rad);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-double _sq (double a, PARAMS){
+double _sq(double a, PARAMS)
+{
   double r = sq(a);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-double _sqrt(double a, PARAMS){
+double _sqrt(double a, PARAMS)
+{
   double r = sqrt(a);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-long _constrain(long amt, long low, long high, PARAMS){
+long _constrain(long amt, long low, long high, PARAMS)
+{
   long r = constrain(amt, low, high);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-double _constrain(double amt, double low, double high, PARAMS){
+double _constrain(double amt, double low, double high, PARAMS)
+{
   double r = constrain(amt, low, high);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
@@ -267,105 +285,119 @@ double _tan(float rad, PARAMS)
   return r;
 }
 
-
 // Characters
 
-boolean _isAlphaNumeric(int c, PARAMS) {
+boolean _isAlphaNumeric(int c, PARAMS)
+{
   boolean b = isAlphaNumeric(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isAlpha(int c, PARAMS){
+boolean _isAlpha(int c, PARAMS)
+{
   boolean b = isAlpha(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isAscii(int c, PARAMS){
+boolean _isAscii(int c, PARAMS)
+{
   boolean b = isAscii(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isWhitespace(int c, PARAMS){
+boolean _isWhitespace(int c, PARAMS)
+{
   boolean b = isWhitespace(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isControl(int c, PARAMS){
+boolean _isControl(int c, PARAMS)
+{
   boolean b = isControl(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isDigit(int c, PARAMS){
+boolean _isDigit(int c, PARAMS)
+{
   boolean b = isDigit(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isGraph(int c, PARAMS){
+boolean _isGraph(int c, PARAMS)
+{
   boolean b = isGraph(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isLowerCase(int c, PARAMS){
+boolean _isLowerCase(int c, PARAMS)
+{
   boolean b = isLowerCase(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isPrintable(int c, PARAMS){
+boolean _isPrintable(int c, PARAMS)
+{
   boolean b = isPrintable(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isPunct(int c, PARAMS){
+boolean _isPunct(int c, PARAMS)
+{
   boolean b = isPunct(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isSpace(int c, PARAMS){
+boolean _isSpace(int c, PARAMS)
+{
   boolean b = isSpace(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isUpperCase(int c, PARAMS){
+boolean _isUpperCase(int c, PARAMS)
+{
   boolean b = isUpperCase(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-boolean _isHexadecimalDigit(int c, PARAMS){
+boolean _isHexadecimalDigit(int c, PARAMS)
+{
   boolean b = isHexadecimalDigit(c);
   printValueFormatted(SerialWrapper(b), id, line, index, items);
   return b;
 }
 
-int _toAscii(int c, PARAMS){
+int _toAscii(int c, PARAMS)
+{
   int i = toAscii(c);
   printValueFormatted(SerialWrapper(i), id, line, index, items);
   return i;
 }
 
-int _toLowerCase(int c, PARAMS){
+int _toLowerCase(int c, PARAMS)
+{
   int i = toLowerCase(c);
   printValueFormatted(SerialWrapper(i), id, line, index, items);
   return i;
 }
 
-int _toUpperCase(int c, PARAMS){
+int _toUpperCase(int c, PARAMS)
+{
   int i = toUpperCase(c);
   printValueFormatted(SerialWrapper(i), id, line, index, items);
   return i;
 }
-
 
 // Serial
 
@@ -405,7 +437,6 @@ void _Serial_print(char value, uint8_t format, PARAMS)
   printValueFormatted(SerialWrapper(value, format), id, line, index, items);
 }
 
-
 // Bits and Bytes
 
 long _bit(long n, PARAMS)
@@ -429,25 +460,20 @@ long _bitRead(long value, uint8_t bit, PARAMS)
   return r;
 }
 
+#define _bitSet(value, bit, id, line, index, items) ({               \
+  bitSet(value, bit);                                                \
+  printValueFormatted(SerialWrapper(value), id, line, index, items); \
+})
 
+#define _bitToggle(value, bit, id, line, index, items) ({            \
+  bitToggle(value, bit);                                             \
+  printValueFormatted(SerialWrapper(value), id, line, index, items); \
+})
 
-
-// void _bitSet(int& x, uint8_t n, PARAMS){}
-// void _bitSet(byte& x, uint8_t n, PARAMS)
-
-// void _bitSet(long& x, uint8_t n, PARAMS)
-// {
-//   bitSet(x, n);
-//   printValueFormatted(SerialWrapper(x), id, line, index, items);
-// }
-
-// void _bitWrite(uint32_t &x, uint8_t n, uint8_t b, PARAMS)
-// {
-//   b ? (x |= (1UL << n)) : bitClear(x, n);
-//   printValueFormatted(SerialWrapper(x), id, line, index, items);
-// }
-
-// bit toggle.........
+#define _bitWrite(value, bit, bitvalue, id, line, index, items) ({   \
+  bitWrite(value, bit, bitvalue);                                    \
+  printValueFormatted(SerialWrapper(value), id, line, index, items); \
+})
 
 uint8_t _highByte(long w, PARAMS)
 {
@@ -462,13 +488,56 @@ uint8_t _lowByte(long w, PARAMS)
   return r;
 }
 
-
-
 // Advanced I/O
-// noTone()
-// shiftIn()
-// shiftOut()
-// tone()
 
+uint8_t _shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, PARAMS)
+{
+  uint8_t r = shiftIn(dataPin, clockPin, bitOrder);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
 
-#endif
+char *int2bin(unsigned int x, uint8_t bitOrder)
+{
+  static char buffer[9];
+  for (int i = 0; i < 8; i++)
+    if (bitOrder == MSBFIRST)
+      buffer[7 - i] = '0' + ((x & (1 << i)) > 0);
+    else if (bitOrder == LSBFIRST)
+      buffer[i] = '0' + ((x & (1 << i)) > 0);
+
+  buffer[8] = '\0';
+  return buffer;
+}
+
+void _shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val, PARAMS)
+{
+  shiftOut(dataPin, clockPin, bitOrder, val);
+  printValueFormatted(SerialWrapper(int2bin(val, bitOrder)), id, line, index, items);
+}
+
+long getFrequency(int pin)
+{
+  // the more sample, the more accurate, yet slower
+#define SAMPLES 3
+  long freq = 0;
+  for (unsigned int j = 0; j < SAMPLES; j++)
+    freq += 500000 / pulseIn(pin, HIGH, 250000);
+  return freq / SAMPLES;
+}
+
+void _tone(uint8_t pin, unsigned int frequency, unsigned long duration, PARAMS)
+{
+  tone(pin, frequency, duration);
+  Serial.println("asdf");
+  long freq = getFrequency(pin);
+  Serial.println("asdf2");
+  printValueFormatted(SerialWrapper(freq), id, line, index, items);
+}
+
+void _tone(uint8_t pin, unsigned int frequency, PARAMS)
+{
+  _tone(pin, frequency, 0, id, line, index, items);
+}
+
+#endif // __DASH_FUNCTIONS__H__
