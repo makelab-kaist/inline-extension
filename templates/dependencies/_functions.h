cce9ed3dfe1dbe01f40c6f386cf38eee45bbb172
Andrea <andrea@kaist.ac.kr>
2022-02-10 13:00:22 +0900

    Added all Arduino functions

--- templates/dependencies/_functions.h
+++ templates/dependencies/_functions.h
@@ -1,8 +1,12 @@
 #ifndef __DASH_FUNCTIONS__H__
 #define __DASH_FUNCTIONS__H__
 
+
 #define PAUSE 5
 #define _Serial_println _Serial_print
+#define _pulseInLong _pulseIn
+
+#define PARAMS char *id, uint16_t line, uint8_t index, uint8_t items
 
 enum ContentType
 {
@@ -16,13 +20,13 @@ class SerialWrapper
 private:
   ContentType tp;
   uint8_t fmt;
-  int i;
+  long i;
   double d;
   char c;
   String s;
 
 public:
-  SerialWrapper(int a, uint8_t format = DEC) : i(a), fmt(format), tp(INTEGER) {}
+  SerialWrapper(long a, uint8_t format = DEC) : i(a), fmt(format), tp(INTEGER) {}
   SerialWrapper(char a, uint8_t format) : s(String(a)), tp(STRING) {} // format will be ignored
   SerialWrapper(double a, uint8_t format) : d(a), tp(FLOAT) {}        // format will be ignored
   SerialWrapper(const String &a) : s(a), tp(STRING) {}
@@ -50,7 +54,8 @@ public:
   }
 };
 
-void printValueFormatted(const SerialWrapper &value, const char *id, uint16_t line, uint16_t index, uint16_t items)
+
+void printValueFormatted(const SerialWrapper &value, PARAMS)
 {
   if (index == 1) // first in line
   {
@@ -72,37 +77,41 @@ void printValueFormatted(const SerialWrapper &value, const char *id, uint16_t li
     Serial.print(",");
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Wrappers
+////////////////////////////////////////////////////////////////////////////////////////
+
 // Pins
 
-void _pinMode(uint16_t pin, uint16_t mode, char *id, uint16_t line, uint16_t index, uint16_t items)
+void _pinMode(uint8_t pin, uint8_t mode, PARAMS)
 {
   pinMode(pin, mode);
   int v = digitalRead(pin);
   printValueFormatted(SerialWrapper(v), id, line, index, items);
 }
 
-int _digitalRead(uint16_t pin, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  int v = digitalRead(pin);
-  printValueFormatted(SerialWrapper(v), id, line, index, items);
-  return v;
-}
-
-void _digitalWrite(uint16_t pin, uint16_t value, char *id, uint16_t line, uint16_t index, uint16_t items)
+void _digitalWrite(uint8_t pin, uint8_t value, PARAMS)
 {
   digitalWrite(pin, value);
   int v = digitalRead(pin);
   printValueFormatted(SerialWrapper(v), id, line, index, items);
 }
 
-int _analogRead(uint16_t pin, char *id, uint16_t line, uint16_t index, uint16_t items)
+int _digitalRead(uint8_t pin, PARAMS)
+{
+  int v = digitalRead(pin);
+  printValueFormatted(SerialWrapper(v), id, line, index, items);
+  return v;
+}
+
+int _analogRead(uint8_t pin, PARAMS)
 {
   int v = analogRead(pin);
   printValueFormatted(SerialWrapper(v), id, line, index, items);
   return v;
 }
 
-void _analogWrite(uint16_t pin, uint16_t value, char *id, uint16_t line, uint16_t index, uint16_t items)
+void _analogWrite(uint8_t pin, int value, PARAMS)
 {
   analogWrite(pin, value);
   unsigned long highTime = pulseIn(pin, HIGH);
@@ -112,179 +121,354 @@ void _analogWrite(uint16_t pin, uint16_t value, char *id, uint16_t line, uint16_
   printValueFormatted(SerialWrapper(perc), id, line, index, items);
 }
 
+
 // Time
 
-unsigned long _millis(char *id, uint16_t line, uint16_t index, uint16_t items)
+unsigned long _millis(PARAMS)
 {
   unsigned long m = millis();
   printValueFormatted(SerialWrapper(m), id, line, index, items);
   return m;
 }
 
-unsigned long _micros(char *id, uint16_t line, uint16_t index, uint16_t items)
+unsigned long _micros(PARAMS)
 {
   unsigned long m = micros();
   printValueFormatted(SerialWrapper(m), id, line, index, items);
   return m;
 }
 
+unsigned long _pulseIn(uint8_t pin, uint8_t state, unsigned long timeout, PARAMS)
+{
+  unsigned long m = pulseIn(pin, state, timeout);
+  printValueFormatted(SerialWrapper(m), id, line, index, items);
+  return m;
+}
+
+
 // Random
 
-long _random(long maxvalue, char *id, uint16_t line, uint16_t index, uint16_t items)
+long _random(long maxvalue, PARAMS)
 {
   long r = random(maxvalue);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-long _random(long minvalue, long maxvalue, char *id, uint16_t line, uint16_t index, uint16_t items)
+long _random(long minvalue, long maxvalue, PARAMS)
 {
   long r = random(minvalue, maxvalue);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-// Serial
-
-// print without formatter
-void _Serial_print(int value, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  printValueFormatted(SerialWrapper(value), id, line, index, items);
+long _map(long x, long in_min, long in_max, long out_min, long out_max, PARAMS) {
+  long m = map(x, in_min, in_max, out_min, out_max);
+  printValueFormatted(SerialWrapper(m), id, line, index, items);
+  return m;
 }
 
-void _Serial_print(double value, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  printValueFormatted(SerialWrapper(value), id, line, index, items);
+
+// Math
+
+long _min (long a, long b, PARAMS){
+  long r = min(a,b);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
+double _min (double a, double b, PARAMS){
+  double r = min(a,b);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
 }
 
-void _Serial_print(char value, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  printValueFormatted(SerialWrapper(value), id, line, index, items);
+long _max (long a, long b, PARAMS){
+  long r = max(a,b);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
 }
 
-void _Serial_print(const String &value, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  printValueFormatted(SerialWrapper(value), id, line, index, items);
+double _max (double a, double b, PARAMS){
+  double r = max(a,b);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
 }
 
-// print with formatter
-void _Serial_print(int value, uint8_t format, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  printValueFormatted(SerialWrapper(value, format), id, line, index, items);
+long _abs (long a, PARAMS){
+  long r = abs(a);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
+double _abs (double a, PARAMS){
+  double r = abs(a);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
 }
 
-void _Serial_print(double value, uint8_t format, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  printValueFormatted(SerialWrapper(value, format), id, line, index, items);
+long _round (double a, PARAMS){
+  long r = round(a);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r; 
 }
 
-void _Serial_print(char value, uint8_t format, char *id, uint16_t line, uint16_t index, uint16_t items)
-{
-  printValueFormatted(SerialWrapper(value, format), id, line, index, items);
+double _radians (double deg, PARAMS){
+  double r = radians(deg);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
 }
 
-// Trigonometry
+double _degrees (double rad, PARAMS){
+  double r = degrees(rad);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
 
-double _cos(float rad, char *id, uint16_t line, uint16_t index, uint16_t items)
+double _sq (double a, PARAMS){
+  double r = sq(a);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
+
+double _sqrt(double a, PARAMS){
+  double r = sqrt(a);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
+
+long _constrain(long amt, long low, long high, PARAMS){
+  long r = constrain(amt, low, high);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
+
+double _constrain(double amt, double low, double high, PARAMS){
+  double r = constrain(amt, low, high);
+  printValueFormatted(SerialWrapper(r), id, line, index, items);
+  return r;
+}
+
+double _cos(float rad, PARAMS)
 {
   double r = cos(rad);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
-double _sin(float rad, char *id, uint16_t line, uint16_t index, uint16_t items)
+double _sin(float rad, PARAMS)
 {
   double r = sin(rad);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-double _tan(float rad, char *id, uint16_t line, uint16_t index, uint16_t items)
+double _tan(float rad, PARAMS)
 {
   double r = tan(rad);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
+
+// Characters
+
+boolean _isAlphaNumeric(int c, PARAMS) {
+  boolean b = isAlphaNumeric(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isAlpha(int c, PARAMS){
+  boolean b = isAlpha(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isAscii(int c, PARAMS){
+  boolean b = isAscii(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isWhitespace(int c, PARAMS){
+  boolean b = isWhitespace(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isControl(int c, PARAMS){
+  boolean b = isControl(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isDigit(int c, PARAMS){
+  boolean b = isDigit(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isGraph(int c, PARAMS){
+  boolean b = isGraph(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isLowerCase(int c, PARAMS){
+  boolean b = isLowerCase(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isPrintable(int c, PARAMS){
+  boolean b = isPrintable(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isPunct(int c, PARAMS){
+  boolean b = isPunct(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isSpace(int c, PARAMS){
+  boolean b = isSpace(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isUpperCase(int c, PARAMS){
+  boolean b = isUpperCase(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+boolean _isHexadecimalDigit(int c, PARAMS){
+  boolean b = isHexadecimalDigit(c);
+  printValueFormatted(SerialWrapper(b), id, line, index, items);
+  return b;
+}
+
+int _toAscii(int c, PARAMS){
+  int i = toAscii(c);
+  printValueFormatted(SerialWrapper(i), id, line, index, items);
+  return i;
+}
+
+int _toLowerCase(int c, PARAMS){
+  int i = toLowerCase(c);
+  printValueFormatted(SerialWrapper(i), id, line, index, items);
+  return i;
+}
+
+int _toUpperCase(int c, PARAMS){
+  int i = toUpperCase(c);
+  printValueFormatted(SerialWrapper(i), id, line, index, items);
+  return i;
+}
+
+
+// Serial
+
+void _Serial_print(long value, PARAMS)
+{
+  printValueFormatted(SerialWrapper(value), id, line, index, items);
+}
+
+void _Serial_print(double value, PARAMS)
+{
+  printValueFormatted(SerialWrapper(value), id, line, index, items);
+}
+
+void _Serial_print(char value, PARAMS)
+{
+  printValueFormatted(SerialWrapper(value), id, line, index, items);
+}
+
+void _Serial_print(const String &value, PARAMS)
+{
+  printValueFormatted(SerialWrapper(value), id, line, index, items);
+}
+
+// print with formatter
+void _Serial_print(long value, uint8_t format, PARAMS)
+{
+  printValueFormatted(SerialWrapper(value, format), id, line, index, items);
+}
+
+void _Serial_print(double value, uint8_t format, PARAMS)
+{
+  printValueFormatted(SerialWrapper(value, format), id, line, index, items);
+}
+
+void _Serial_print(char value, uint8_t format, PARAMS)
+{
+  printValueFormatted(SerialWrapper(value, format), id, line, index, items);
+}
+
+
 // Bits and Bytes
 
-uint32_t _bit(uint8_t n, char *id, uint16_t line, uint16_t index, uint16_t items)
+long _bit(long n, PARAMS)
 {
-  uint32_t r = bit(n);
+  long r = bit(n);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-uint32_t _bitClear(uint32_t x, uint8_t n, char *id, uint16_t line, uint16_t index, uint16_t items)
+long _bitClear(long value, uint8_t bit, PARAMS)
 {
-  uint32_t r = bitClear(x, n);
+  long r = bitClear(value, bit);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-uint32_t _bitRead(uint32_t x, uint8_t n, char *id, uint16_t line, uint16_t index, uint16_t items)
+long _bitRead(long value, uint8_t bit, PARAMS)
 {
-  uint32_t r = bitRead(x, n);
+  long r = bitRead(value, bit);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-// void _bitSet(uint32_t &x, uint8_t n, char *id, uint16_t line, uint16_t index, uint16_t items)
+
+
+
+// void _bitSet(int& x, uint8_t n, PARAMS){}
+// void _bitSet(byte& x, uint8_t n, PARAMS)
+
+// void _bitSet(long& x, uint8_t n, PARAMS)
 // {
-//   x |= (1UL << n);
+//   bitSet(x, n);
 //   printValueFormatted(SerialWrapper(x), id, line, index, items);
 // }
 
-// void _bitWrite(uint32_t &x, uint8_t n, uint8_t b, char *id, uint16_t line, uint16_t index, uint16_t items)
+// void _bitWrite(uint32_t &x, uint8_t n, uint8_t b, PARAMS)
 // {
 //   b ? (x |= (1UL << n)) : bitClear(x, n);
 //   printValueFormatted(SerialWrapper(x), id, line, index, items);
 // }
 
-uint8_t _highByte(uint16_t x, char *id, uint16_t line, uint16_t index, uint16_t items)
+// bit toggle.........
+
+uint8_t _highByte(long w, PARAMS)
 {
-  uint8_t r = highByte(x);
+  uint8_t r = highByte(w);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
-uint8_t _lowByte(uint16_t x, char *id, uint16_t line, uint16_t index, uint16_t items)
+uint8_t _lowByte(long w, PARAMS)
 {
-  uint8_t r = lowByte(x);
+  uint8_t r = lowByte(w);
   printValueFormatted(SerialWrapper(r), id, line, index, items);
   return r;
 }
 
-// Math
-// TODO
 
-// abs()
-// constrain()
-// map()
-// max()
-// min()
-// pow()
-// sq()
-// sqrt()
 
 // Advanced I/O
 // noTone()
-// pulseIn()
-// pulseInLong()
 // shiftIn()
 // shiftOut()
 // tone()
 
-// Characters
-// isAlpha()
-// isAlphaNumeric()
-// isAscii()
-// isControl()
-// isDigit()
-// isGraph()
-// isHexadecimalDigit()
-// isLowerCase()
-// isPrintable()
-// isPunct()
-// isSpace()
-// isUpperCase()
-// isWhitespace()
+
 #endif
