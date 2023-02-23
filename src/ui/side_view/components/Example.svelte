101bbc5f05a35e28d8b14c4478e450b3d6021bb9
Andrea <andrea@kaist.ac.kr>
2023-02-23 18:35:15 +0900

    WIP

---
+++ src/ui/side_view/components/Example.svelte
@@ -0,0 +1,53 @@
+<script>
+  export let expression = '';
+  export let result = '';
+
+  function copyToClipboard() {
+    console.log(expression);
+    navigator.clipboard.writeText(expression + ' ?');
+  }
+</script>
+
+<p class="expression">
+  {expression}
+  <span class="result">
+    <b class="op">?</b>
+    {result}</span
+  >
+  <button on:click={copyToClipboard}>copy</button>
+</p>
+
+<style>
+  p {
+    display: flex;
+
+    margin: 0px;
+    margin-bottom: 0.2em;
+  }
+
+  .expression {
+    background-color: black;
+    color: grey;
+    padding: 0.5em;
+    flex-grow: 4;
+  }
+
+  .result {
+    color: dodgerblue;
+  }
+
+  .op {
+    margin-left: 0.3em;
+    margin-right: 0.5em;
+  }
+
+  button {
+    width: auto;
+    border-radius: 20%;
+    margin-left: auto;
+  }
+
+  button:hover {
+    background-color: dodgerblue;
+  }
+</style>
